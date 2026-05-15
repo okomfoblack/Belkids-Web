60c9fc7dd43ce48e25a2d15a053835d86443fe0e
okomfoblack <okomfoblack@gmail.com>
2026-05-15 12:17:08 +0000

    first commit

---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+ 
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
