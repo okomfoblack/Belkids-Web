ad2bd06d7bc9e28b6802a9b17dd1fa5f2498ff80
okomfoblack <okomfoblack@gmail.com>
2026-05-21 15:15:29 +0000

    Add Hello World route to the Express server

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
- 
+
 dotenv.config();
 
 const app = express();
+
+// Route to print Hello World
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
