30279411321988e847896534d552040d100be3f1
okomfoblack <okomfoblack@gmail.com>
2026-08-28 15:31:47 +0000

    Fix import case sensitivity for User model in authController

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -2,7 +2,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import User from "../models/User.js";
+import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
